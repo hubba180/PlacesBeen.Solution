3fa690625d3becd245b0f65f2d0ccab8473697c6
Sara Kane <kane.sarak@gmail.com>
2020-07-22 11:59:46 -0700

    Add PlacesController, Places Index and a add place page
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

---
+++ PlacesBeen/Controllers/PlacesController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using PlacesBeen.Models;
+using System.Collections.Generic;
+
+namespace PlacesBeen.Controllers
+{
+  public class PlacesController : Controller
+  {
+    [HttpGet("/places")]
+    public ActionResult Index()
+    {
+      List<Place> allPlaces = Place.GetAll();
+      return View(allPlaces);
+    }
+  }
+}
