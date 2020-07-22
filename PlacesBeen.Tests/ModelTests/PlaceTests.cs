6977dce07f9fc3dab0e587248935957446beeb23
Sara Kane <kane.sarak@gmail.com>
2020-07-22 10:33:30 -0700

    Add passing test for Place constructor
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

--- PlacesBeen.Tests/ModelTests/PlaceTests.cs
+++ PlacesBeen.Tests/ModelTests/PlaceTests.cs
@@ -6,6 +6,11 @@ namespace PlacesBeen.Tests
   [TestClass]
   public class PlaceTest
   {
-    
+    [TestMethod]
+    public void PlaceConstructor_CreatesInstanceOfPlace_Place()
+    {
+      Place newPlace = new Place("Portland");
+      Assert.AreEqual(typeof(Place), newPlace.GetType());
+    }
   }
 }
--- PlacesBeen/Models/Place.cs
+++ PlacesBeen/Models/Place.cs
@@ -2,6 +2,11 @@ namespace PlacesBeen.Models
 {
   public class Place
   {
-    
+    public string CityName { get; set; }
+
+    public Place(string cityName)
+    {
+      CityName = cityName;
+    }
   }
 }
